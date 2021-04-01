1e2d4a276f9925c2680b8df6ad7ea7586c3d967d
ssEmKbyI <ssemkbyi@email.com>
2021-04-01 13:35:08 +0000

    Update Task1.java

--- src/main/java/com/epam/test/automation/java/practice4/Task1.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task1.java
@@ -4,27 +4,20 @@ public class Task1 {
 
     
     public static boolean isSorted(int[] array, SortOrder order) {
-        if (order == SortOrder.ASC)
-        {
-            for (int i = 0; i < array.length - 1; i++)
-            {
-                if (array[i] > array[i + 1])
-                {
+        if (array == null || array.length ==0) throw new IllegalArgumentException();
+        else if (order == SortOrder.Ascending) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] < array[i + 1]) {
                     return false;
                 }
             }
         }
-        else if (order == SortOrder.DESC)
-        {
-            for (int i = 0; i < array.length - 1; i++)
-            {
-                if (array[i] < array[i + 1])
-                {
-                    return false;
-                }
-            }
-        } else throw new IllegalArgumentException();
-        
         return true;
     }
 }
