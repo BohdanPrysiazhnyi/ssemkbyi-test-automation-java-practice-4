b1f05fe83254af81d6de5fa8f86d8c5a2aa851d6
ssEmKbyI <ssemkbyi@email.com>
2021-04-01 12:37:25 +0000

    Update Task2.java

--- src/main/java/com/epam/test/automation/java/practice4/Task2.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task2.java
@@ -4,7 +4,7 @@ public class Task2 {
 
    
     public static int[] transform(int[] array, SortOrder order) {
-        if (IsSorted(array, order))
+        if (isSorted(array, order))
             {
                 for (int i = 0; i < array.length; i++)
                 {
