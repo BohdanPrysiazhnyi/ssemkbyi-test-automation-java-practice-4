2bb0f0efd0d5c1dd5f7e415b5eb7be3e94cda209
ssEmKbyI <ssemkbyi@email.com>
2021-04-01 11:48:57 +0000

    Update Task1.java

--- src/main/java/com/epam/test/automation/java/practice4/Task1.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
     
     public static boolean isSorted(int[] array, SortOrder order) {
       
-        if (order == SortOrder.Ascending)
+        if (order == SortOrder.Asc)
         {
             for (int i = 0; i < array.length - 1; i++)
             {
