7e1d0fbe88e50f94c0f08172bf487dc43f19c72d
ssEmKbyI <ssemkbyi@email.com>
2021-04-01 12:43:50 +0000

    Update Task1.java

--- src/main/java/com/epam/test/automation/java/practice4/Task1.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task1.java
@@ -4,7 +4,7 @@ public class Task1 {
 
     
     public static boolean isSorted(int[] array, SortOrder order) {
-        if (array == null) throw new NullPointerException();
+        if (array == null) throw new UnsupportedOperationException();
         else if (order == SortOrder.ASC)
         {
             for (int i = 0; i < array.length - 1; i++)
