204150428968d2936441d62fb056df848f4b3621
ssEmKbyI <ssemkbyi@email.com>
2021-04-01 14:33:09 +0000

    Update Task3.java

--- src/main/java/com/epam/test/automation/java/practice4/Task3.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task3.java
@@ -2,16 +2,9 @@ package com.epam.test.automation.java.practice4;
 
 public class Task3 {
 
-    /**
-     * <summary>
-     * Implement code according to description of task.
-     * </summary>
-     * if set invalid arguments in method, then method must throws
-     * IllegalArgumentException
-     */
     public static int multiArithmeticElements(int a1, int t, int n) {
           int result = 0;
-          if (n == 0 || n < 0 ) {throw new UnsupportedOperationException();}
+          if (n == 0 || n < 0 ) {throw new IllegalArgumentException();}
           else {
               int n1 = n -1;
               int t1 = t*n1;
