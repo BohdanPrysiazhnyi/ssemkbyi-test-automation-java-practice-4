0c3044a9a1d9ac10a300141e6e8fbf02f3220b56
ssEmKbyI <ssemkbyi@email.com>
2021-04-01 11:46:13 +0000

    Update Task1.java

--- src/main/java/com/epam/test/automation/java/practice4/Task1.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task1.java
@@ -2,14 +2,29 @@ package com.epam.test.automation.java.practice4;
 
 public class Task1 {
 
-    /**
-     * <summary>
-     * Implement code according to description of task.
-     * </summary>
-     * if set invalid arguments in method, then method must throws
-     *  IllegalArgumentException
-     */
+    
     public static boolean isSorted(int[] array, SortOrder order) {
-        throw new UnsupportedOperationException();
+      
+        if (order == SortOrder.Ascending)
+        {
+            for (int i = 0; i < array.length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < array.length - 1; i++)
+            {
+                if (array[i] < array[i + 1])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
