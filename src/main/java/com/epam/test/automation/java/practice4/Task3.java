81c5c57669c66310cffde0511bf17c7a6d6975b1
ssEmKbyI <ssemkbyi@email.com>
2021-04-02 07:19:15 +0000

    Update Task3.java

--- src/main/java/com/epam/test/automation/java/practice4/Task3.java
+++ src/main/java/com/epam/test/automation/java/practice4/Task3.java
@@ -3,14 +3,16 @@ package com.epam.test.automation.java.practice4;
 public class Task3 {
 
     public static int multiArithmeticElements(int a1, int t, int n) {
-          int result = 0;
-          int currentNumber = a1;
-          if (n == 0 || n < 0 ) {throw new IllegalArgumentException();}
-          else {
-             for (int i = 0; i < n; i++) {
-                  currentNumber *=t;
-                  result = a1 + currentNumber;
-          }
+          int result = a1;
+        int currentNumber = a1;
+        if (n == 0 || n < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            for (int i = 1; i < n; i++) {
+                currentNumber += t;
+                result *= currentNumber;
+            }
+
           return result;
         }
     }
